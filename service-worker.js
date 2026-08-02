2c08568737f10d7f74875c6f8a19633cfdb99deb
m4xon007 <103837048+m4xon007@users.noreply.github.com>
2026-08-02 17:24:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 // MTracker PWA — offline cache
 // Zmień CACHE_VERSION przy każdym deployu żeby wymusić odświeżenie cache
-const CACHE_VERSION = 'mtracker-v3.39';
+const CACHE_VERSION = 'mtracker-v4.1';
 const CACHE_NAME = CACHE_VERSION;
 
 // Zasoby do pre-cache przy instalacji
